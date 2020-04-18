d0496087c1b149af16e5c60ba7fee6b4ab7e2283
Igor Pinheiro <igor.warlog@gmail.com>
2020-04-18 14:04:28 -0300

    script scanner de portas simples funcional

---
+++ scan-port.ps1
@@ -0,0 +1,17 @@
+﻿param($ip)
+
+if (!$ip){
+     
+     echo "IGOR PINHEIRO - PORTSCAN"
+     
+     echo ".\scan-port.sp1 192.168.0.1"
+} else {
+$topports = 21,22,25,3306,80,8081,443,3000,30000,500,100,111
+try {foreach ($port in $topports){
+if (Test-NetConnection $ip -Port $port -WarningAction SilentlyContinue -InformationLevel Quiet) {
+
+    echo "Port Open: $port"
+}} else {
+    echo "Port Closed: $port"
+}} catch{}
+}
