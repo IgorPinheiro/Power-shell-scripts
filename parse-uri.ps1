26b031873948db3f3aa4a82038b603aab8cb65a2
Igor Pinheiro <igor.warlog@gmail.com>
2020-04-23 11:12:14 -0300

    Identação do código parse uri

--- parse-uri.ps1
+++ parse-uri.ps1
@@ -1,20 +1,25 @@
 $site = Read-Host "Digite o site: "
+
 if (!$site){
     echo "Parse URI - By: Igor Pinheiro"
     echo "Site não encontrado!"
     echo "Execute novamente e digite o site"
 }else{
     try{
-    $web = Invoke-WebRequest -uri "$site" -Method Options
+        $web = Invoke-WebRequest -uri "$site" -Method Options
 
-    echo "O Servidor Roda: "
-    $web.headers.server
-    echo ""
-    echo "O servidor aceita os metodos: "
-    $web.headers.allow
-    echo ""
-    echo "Links Encontrados"
-    $web2 = Invoke-WebRequest -uri "$site"
-    $web2.links.href | Select-String http://
+        echo "O Servidor Roda: "
+        $web.headers.server
+        
+        echo ""
+        
+        echo "O servidor aceita os metodos: "
+        $web.headers.allow
+        
+        echo ""
+        
+        echo "Links Encontrados"
+        $web2 = Invoke-WebRequest -uri "$site"
+        $web2.links.href | Select-String http://
     } catch{}
 }
