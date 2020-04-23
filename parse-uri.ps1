844f725b7db36eb39ce91112b270322f87f3bba9
Igor Pinheiro <igor.warlog@gmail.com>
2020-04-23 11:04:54 -0300

    Script em powershell para parse uri

---
+++ parse-uri.ps1
@@ -0,0 +1,20 @@
+$site = Read-Host "Digite o site: "
+if (!$site){
+    echo "Parse URI - By: Igor Pinheiro"
+    echo "Sinte não encontrado!"
+    echo "Execute novamente e digite o site"
+}else{
+    try{
+    $web = Invoke-WebRequest -uri "$site" -Method Options
+
+    echo "O Servidor Roda: "
+    $web.headers.server
+    echo ""
+    echo "O servidor aceita os metodos: "
+    $web.headers.allow
+    echo ""
+    echo "Links Encontrados"
+    $web2 = Invoke-WebRequest -uri "$site"
+    $web2.links.href | Select-String http://
+    } catch{}
+}
